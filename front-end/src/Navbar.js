924e3c2c71d8e34455ae20a62aed83550ce85573
cxl229 <cxl229@nyu.edu>
2023-03-01 12:23:58 -0500

    created navbar component and render it in app.js

---
+++ front-end/src/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import './Navbar.css';
+
+const Navbar= () =>{
+  return (
+  <div className="navbar">
+    <Link to="/" className="link">
+      <li>Meal plan</li>
+    </Link>
+    <Link to="/recipesearch" className="link">
+      <li>Recipe search</li>
+    </Link>
+    <Link to="/savedrecipes" className="link">
+      <li>Saved recipes</li>
+    </Link>
+    <Link to="/fridge" className="link">
+      <li>Fridge</li>
+    </Link>
+    <Link to="/grocerylist" className="link">
+      <li>Grocery list</li>
+    </Link>
+  </div>
+  );
+}
+
+export default Navbar;
