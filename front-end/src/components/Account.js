717f4f7be65752eaaaf3741ce42dae590cd50aae
aleezaw <aleezaw@gmail.com>
2023-03-05 20:21:58 -0500

    create account js page

---
+++ front-end/src/components/Account.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Account.css';
+
+const Account = () => {
+    return (
+        <div className='Account'>
+            <h1>Temporary for Profile Picture Placement</h1>
+            <p>Name: </p>
+        </div>
+
+    );
+
+}
+
+export default Account;
