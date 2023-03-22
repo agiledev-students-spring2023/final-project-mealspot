49a6a588e0ae0473c89accbb9c048eefbf0f2161
kxg202 <kevinxgong@gmail.com>
2023-03-21 21:14:30 -0400

    Added scroll feature

--- front-end/src/components/FridgeItem.js
+++ front-end/src/components/FridgeItem.js
@@ -3,10 +3,10 @@ import {Button} from '@mui/material';
 import './FridgeItem.css';
 
 const buttonStyle = {
-    border: 1,
-    height: 30,
-    marginTop: '14px',
-    left: '75%'
+  border: 1,
+  height: 30,
+  marginTop: '18px',
+  left: '75%'
 }
 
 function FridgeItem({id, name, quantity}){
