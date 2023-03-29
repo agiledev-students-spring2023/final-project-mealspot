16160cc9fddb683056e1c62a635ab521ffd70278
cxl229 <cxl229@nyu.edu>
2023-03-29 13:34:40 -0400

    working get route for saved recipes page, added .env in front end

--- front-end/src/components/SavedRecipes.js
+++ front-end/src/components/SavedRecipes.js
@@ -3,12 +3,14 @@ import RecipeDisplay from './RecipeDisplay.js';
 import './SavedRecipes.css';
 
 const SavedRecipes = () => {
+    const apiLink = `${process.env.REACT_APP_SERVER_HOSTNAME}/savedrecipes`;
+
     // Return the final component, consisting of page header and the array of recipe cards
     return (
         <>
         <h1>Saved Recipes</h1>
         <div>
-            <RecipeDisplay apiLink='https://my.api.mockaroo.com/recipe.json?key=8198c2b0' />
+            <RecipeDisplay apiLink = {apiLink} />
         </div>
         </>
     );
