67142bd7fa428b16ef29a21d38b5b977d2ce5ffd
cxl229 <cxl229@nyu.edu>
2023-04-16 15:49:19 -0400

    add ingredient schema

---
+++ back-end/models/Ingredient.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Mongoose model of an ingredient, includes an ID number that corresponds to that ingredient as well as the quantity of ingredient
+const IngredientSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    id: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+})
+
+// create a model from this schema
+const Ingredient = mongoose.model("Ingredient", IngredientSchema);
+
+// export the model
+module.exports = Ingredient;
