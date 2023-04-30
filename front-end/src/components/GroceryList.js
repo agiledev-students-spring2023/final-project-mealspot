787d1c41fe1198c91d7ea710475a9d0872a4f795
cxl229 <cxl229@nyu.edu>
2023-04-30 15:28:24 -0400

    changed button on fridge and grocery list items

--- front-end/src/components/GroceryList.js
+++ front-end/src/components/GroceryList.js
@@ -1,6 +1,6 @@
 import {useState, useEffect, React} from 'react';
 import axios from 'axios';
-import {Modal, Box, Button, Typography, FormControl, TextField} from '@mui/material';
+import {Modal, Box, Button, Typography, FormControl, TextField } from '@mui/material';
 import {useStateValue} from '../StateManager.js';
 import './GroceryList.css';
 import GroceryListItem from './GroceryListItem';
