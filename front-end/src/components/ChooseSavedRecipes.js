472723aec646393e24c2b0822c65f576dd2b2430
cxl229 <cxl229@nyu.edu>
2023-04-30 17:30:37 -0400

    updated choose saved X-out button to fit with the new header

--- front-end/src/components/ChooseSavedRecipes.js
+++ front-end/src/components/ChooseSavedRecipes.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { IconContext } from "react-icons";
-import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import { BsXCircleFill } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 import RecipeDisplay from './RecipeDisplay.js';
@@ -37,11 +36,10 @@ const ChooseSavedRecipes = () => {
      // Return the final component, consisting of page header and the array of recipe cards
      if (isLoading) {
         return (
-            <div className = "popup"> 
+            <div> 
                 <div>
                     <Link to="/" className="backButton">
-                        <IconContext.Provider value={{ size: "3em", color: "black" }}>
-                            {/* <div><BsFillArrowLeftCircleFill /></div> */}
+                        <IconContext.Provider value={{ className: "backIcon" }}>
                             <div><BsXCircleFill /></div>
                         </IconContext.Provider>
                     </Link>
@@ -56,10 +54,10 @@ const ChooseSavedRecipes = () => {
         );
      } else {
         return (
-        <div className = "popup"> 
+        <div> 
             <div>
                 <Link to="/" className="backButton">
-                    <IconContext.Provider value={{ size: "3em", color: "black"}}>
+                    <IconContext.Provider value={{ className: "backIcon" }}>
                         {/* <div><BsFillArrowLeftCircleFill /></div> */}
                         <div><BsXCircleFill /></div>
                     </IconContext.Provider>
